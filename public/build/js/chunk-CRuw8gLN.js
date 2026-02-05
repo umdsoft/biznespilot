5c262964c94953c136fb13b72acc8c51af5be37e
umdsoft <ujumaniyozov@ya.ru>
2026-02-06 00:01:00 +0500

    ASD

--- public/build/js/chunk-CRuw8gLN.js
+++
@@ -1,1 +0,0 @@
-import{C as o,D as r,y as t}from"./chunk-CpmtkKLp.js";function n(n,e){return r(),o("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor","aria-hidden":"true","data-slot":"icon"},[t("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M5 12h14"})])}export{n as r};
--- public/build/js/chunk-Dz84kaPs.js
+++
@@ -1,1 +0,0 @@
-import{C as o,y as r,D as t}from"./chunk-CpmtkKLp.js";function n(n,e){return t(),o("svg",{xmlns:"http://www.w3.org/2000/svg",fill:"none",viewBox:"0 0 24 24","stroke-width":"1.5",stroke:"currentColor","aria-hidden":"true","data-slot":"icon"},[r("path",{"stroke-linecap":"round","stroke-linejoin":"round",d:"M4.5 10.5 12 3m0 0 7.5 7.5M12 3v18"})])}export{n as r};
